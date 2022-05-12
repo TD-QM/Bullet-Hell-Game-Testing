3bfa2046eef5ab215c6aa41a7d8a5ed0f0680eab
Kristopher <44072394+TD-QM@users.noreply.github.com>
2022-05-12 11:22:33 -0500

    Create Enemy.java

---
+++ Testing Game/Enemy.java
@@ -0,0 +1,69 @@
+import java.awt.*;
+
+public class Enemy extends Triangle {
+
+  private int x, y, width, height, dx, dy;
+  private Color color;
+  private boolean alive;
+
+  public Enemy(int x, int y, int width, int height, int dx, int dy, Color color) {
+    super(x, y, x + width, y, x + width / 2, y + height);
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.dx = dx;
+    this.dy = dy;
+  }
+
+  public void tick() {
+    x += dx;
+    y += dy;
+    super.setPointA(x, y);
+    super.setPointB(x + width, y);
+    super.setPointC(x + width / 2, y + height);
+  }
+
+  public void draw(Graphics g) {
+    super.fill(g, color);
+    // super.sketch(g);
+  }
+
+  public void setDx(int dx) {
+    this.dx = dx;
+  }
+
+  public void setDy(int dy) {
+    this.dy = dy;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
+  public int getHeight() {
+    return height;
+  }
+
+  public void spawn() {
+    alive = true;
+  }
+
+  public void die() {
+    alive = false;
+  }
+
+  public boolean aliveCheck() {
+    return alive;
+  }
+
+}
